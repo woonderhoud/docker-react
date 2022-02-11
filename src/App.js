8f5d5a36cc0f8effe3aa5b8d123efd841749b8b1
Jeroen op 't Hoog <jeroen@woonderhoud.nl>
2022-02-11 19:56:26 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You're the best! :D
         </p>
         <a
           className="App-link"
